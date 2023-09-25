187cfcc7042bdcfce89d856099a1f369a449ec8c
ahaieva] <ahayeva.anna@icloud.com>
2023-09-25 16:59:19 +0300

    аіааіа

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    document.addEventListener("DOMContentLoaded", ()=>{
-    const FORM = document.querySelector("#form")
+    const FORM = document.getElementById("form")
     FORM.addEventListener("submit", (e)=>{
         e.preventDefault()
-        const USER_NAME = document.getElementById("userName").value
-        const USER_SURNAME = document.getElementById("userSurname").value
-        const USER_EMAIL  =  document.getElementById("userMail").value
-        console.log(USER_NAME, "____", USER_SURNAME, "____", USER_EMAIL )
-                        
+        const USER_NAME = document.querySelector("#userName").value
+        const USER_SURNAME = document.querySelector("#userSurname").value
+        const USER_EMAIL = document.querySelector("#userEmail").value
+        const USER_PASSWORD = document.querySelector("#userPasswordOne").value
+        const USER_PASSWORD_REPEAT = document.querySelector("#userPasswordTwo").value
+        if(USER_PASSWORD===USER_PASSWORD_REPEAT){
+            console.log(USER_NAME, USER_SURNAME, USER_EMAIL, USER_PASSWORD)
+        }else{
+            alert("Паролі не збігаються")
+        }
     })
-
-})
 })
 
