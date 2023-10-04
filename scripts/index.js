edf70c3dfb8e97f72df3f14aea7f53d4d38d8574
school computer <aslesyk@gmail.com>
2023-10-04 19:35:54 +0300

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
+/*allalaallaal
+i can write anything i want
+lalalalalalal
+bim bim bam bam  
+*/
+
+
+
+
 const getAllProducts = async () => {
     return await fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
@@ -7,25 +16,25 @@ const getAllProducts = async () => {
         })
         .catch(err => console.log(err))
 
-}
+};
 
-const showCounterCart = () =>{
-    // get cart cfrom LS
-    const CART=  localStorage.getItem("cart");
-    if(CART){
-        //  get element 
+const showCounterCart = () => {
+    // get cart from LS
+    const CART = localStorage.getItem("cart");
+    if (CART) {
+        // get element 
         const COUNTER = document.querySelector(".products__cart_counter")
-        // calc qty 
+        // calc qty products
         let allProductsQty = 0;
-        JSON.parse(CART).map((e)=>{
-            allProductsQty += e.qty
+        JSON.parse(CART).map((e) => {
+            allProductsQty += e.qty;
         })
-        // display qty
+        // display qty 
         COUNTER.innerHTML = allProductsQty;
-    }else{
+    } else {
         return
-    }
-}
+    };
+};
 
 const showProductsOnDOM = async (products, selector) => {
     // create html
@@ -43,6 +52,7 @@ const showProductsOnDOM = async (products, selector) => {
             <button data-id=${id} class="add_to_cart">
                 Add to Cart
             </button>
+            <button>Delete from Cart</button>
         </div>
             `
         })
@@ -60,7 +70,7 @@ const showProductsOnDOM = async (products, selector) => {
             addProductsToCart(PRODUCT_ID)
         })
     })
-}
+};
 
 const addNewProduct = async () => {
     await fetch('https://fakestoreapi.com/products', {
@@ -78,7 +88,7 @@ const addNewProduct = async () => {
         .then(res => res.json())
         .then(json => console.log(json))
         .catch(err => console.log(err))
-}
+};
 
 const filterBySearchQuery = (products, query, selector_area) => {
     let filteredProducts = products.filter(item => item.title.toLowerCase().includes(query.toLowerCase()))
@@ -94,7 +104,7 @@ const sortProducts = (products, type, selector_area) => {
         sortedProducts = products.sort((a, b) => b.price - a.price)
     }
     showProductsOnDOM(sortedProducts, selector_area)
-}
+};
 
 const addProductsToCart = (product_id) => {
     // get cart from LS
@@ -104,57 +114,85 @@ const addProductsToCart = (product_id) => {
     // check if cart empty
     if (cart.length > 0) {
         let index = cart.findIndex(product => product_id == product.id)
-        console.log(index)
-        if (index != -1){
+        // check if exist product in cart
+        if (index != -1) {
             cart[index].qty++;
-
-        }else{
+        } else {
             cart.push({
                 id: product_id,
                 qty: 1
             })
+
         }
     } else {
         cart.push({ id: product_id, qty: 1 })
-    }
+    };
     // write to LS
-    localStorage.setItem("cart", JSON.stringify(cart))
+    localStorage.setItem("cart", JSON.stringify(cart));
     // display counter cart
-    
     showCounterCart();
-
-}
+};
 
 // cleaner LS
 document.addEventListener("DOMContentLoaded", () => {
     localStorage.removeItem('dynamic_products')
-})
-
+});
 
 // start point
-
 document.addEventListener("DOMContentLoaded", async () => {
-    showCounterCart();
-    // get elements DOM
-    const INPUT_SELECT = document.querySelector("#products__sort")
-    const INPUT_SEARCH = document.querySelector("#filter_search")
+    // // get path
+    // const PATH = window.location.pathname;
 
+    // showCounterCart();
+    // // set interval
+    // setInterval(()=>{
+    //     console.log("test interval")
+    // }, 2000)
+    // set timeout
+    // setTimeout(()=>{
+    //     console.log("test timeout")
+    // }, 3000)
+    // get elements DOM
+    const INPUT_SELECT = document.querySelector("#products__sort");
+    const INPUT_SEARCH = document.querySelector("#filter_search");
+    const REFRESH_PAGE_BTN = document.querySelector(".refresh_page");
+    const ADD_Q_BTN = document.querySelector(".add_q_params");
+    const READ_Q_BTN = document.querySelector(".read_q_params");
     // get all products
     const PRODUCTS = await getAllProducts();
     // display products
-    showProductsOnDOM(PRODUCTS, ".products__area")
+    showProductsOnDOM(PRODUCTS, ".products__area");
 
     // add event listeners
     INPUT_SELECT.addEventListener("change", (e) => {
-        // get dynamic products
+        // get dynamic products;
         let current_products;
-        const DYNAMIC_PRODUCTS = localStorage.getItem("dynamic_products")
-        DYNAMIC_PRODUCTS ? current_products = JSON.parse(DYNAMIC_PRODUCTS) : current_products = PRODUCTS
-        sortProducts(current_products, e.target.value, ".products__area")
+        const DYNAMIC_PRODUCTS = localStorage.getItem("dynamic_products");
+        DYNAMIC_PRODUCTS ? current_products = JSON.parse(DYNAMIC_PRODUCTS) : current_products = PRODUCTS;
+        sortProducts(current_products, e.target.value, ".products__area");
     })
     INPUT_SEARCH.addEventListener("input", (e) => {
-        filterBySearchQuery(PRODUCTS, e.target.value, ".products__area")
+        filterBySearchQuery(PRODUCTS, e.target.value, ".products__area");
     })
+    REFRESH_PAGE_BTN.addEventListener("click", (e) => {
+        location.reload(); // reload page
+        // location.href = "https://rozetka.com.ua/ua/";
+        // window.open("https://rozetka.com.ua/ua/", "_blank") // open new tab
+        // window.close();
+    })
+    ADD_Q_BTN.addEventListener("click", (e)=>{
+        const url = new URL(location.href);
+        const queryParams = url.searchParams;
+        queryParams.set("name", "Jane");
+        queryParams.set("age", 21);
+        window.open(`${url.href}`, "_self");
+    });
+    READ_Q_BTN.addEventListener("click", ()=>{
+        const url = new URL(location.href);
+        const queryParams = url.searchParams;
+        const name = queryParams.get('name');
+        const age = queryParams.get('age');
+        console.log(name, age);
+    });
 
-})
-
+});
