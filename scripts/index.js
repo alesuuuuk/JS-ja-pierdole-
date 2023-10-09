ec3631dc83be6019786614a40d8b10cb19d3ea8e
school computer <aslesyk@gmail.com>
2023-10-09 19:30:46 +0300

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,102 +1,44 @@
+const getUserData = () => {
+    //     let userName = prompt("Enter your name:");
+    //     // console.log(userName);
+    // //     let test3 = 24
 
-console.log("JA PIERDOLE")
-const KURWA = "kurwa"
-if (KURWA == "fgfgff") {
-    console.log("jfff")
-}
-else {
-    console.log("pacany buryu narygav")
-}
-let text = "some text__"
-
-
-
-var testVariableOne = "test1"
-let testVariableTwo = "test2"
-const testVariableThree = "test3"
-
-// let nameUser, age, surname;
-// nameUser = 'John'
-// age = 21
-// surname = "Doe"
-
-// console.log(nameUser + surname + age)   
-
-// console.log(`Name:${nameUser}, Username${surname}, Age:${age}`)
-// console.log(typeof nameUser, typeof age, typeof surname)
-// console.log(nameUser.toLowerCase()) // toUpperCase()
-// console.log(text.length)
-
-// console.log(text.replaceAll(" ", ""))
-
-// console.log(nameUser[0])
-
-// console.log(56>3)
-
-// let tempArr = [1, 3, 2, 5, 543242, 123, 3425, "test"]
-// console.log(tempArr)
-// console.log(tempArr.length)
-// tempArr.push("new element!!")
-// console.log(tempArr)
-// tempArr.unshift(" new start element")
-// console.log(tempArr)
-// tempArr.pop()
-// console.log(tempArr)
-
-// tempArr.forEach(element => {
-//     console.log(element)
-// });
-
-// let index = tempArr.indexOf('test')
-// tempArr.splice(index, 1)
-// console.log(tempArr)
+    // //     let test = true || alert("test")
+    // //     let test2 = false || alert("test2")
+    //  let dataServer = undefined;
+    //  some code
+    // let localData = dataServer || "Дані не знайдено"
+    // console.log(localData)
+    // let userAge = prompt("enter your age")
+    // // userAge > 18 || alert("Ви не повнолітні")
+    // userAge < 18 && alert('Ви не повнолітні')
+    // let userName = prompt("Enter your name:") || alert("u don't entered name");
+   
 
 
-// let fruits_arr = ['apple', "pineaple", "banana"]
-// fruits_arr.forEach((fruit, index)=>{
-//     console.log(fruit, index)
-// })
-
-// fruits_arr.map((fruit, index)=>{
-//     console.log(fruit, index)
-// })
-
-
-// let filteredFruits = fruits_arr.filter((fruit)=>{
-//     if( fruit == 'banana'){
-//         return fruit
-//     }
-// })
-// console.log(filteredFruits)
-
-
-// filteredFruits = fruits_arr.filter(fruit => fruit== 'apple')
-// console.log(filteredFruits)
-
-
-// // sort
-// let numbersArr = [1,2,33,423,4,24,42,42,32,4,24,35,654,67]
-// numbersArr.sort((a, b)=>{
-//     return a - b
-// })
-
-// console.log(numbersArr)
-
-
-let tempUser = {
-    name: "john",
-    age: 21,
-    adress: [],
-    surname: "doe"
+    // get user data
+    let userName = document.querySelector(".form__name").value
+    userName ? hideErrorStatus(".form__name", ".form__name_error") : displayErrorStatus(".form__name", ".form__name_error", "u don't wrote name")
+   
 }
 
-console.log(tempUser.name)
-// add new element
-tempUser.salary = 3000
-console.log(tempUser)
+const displayErrorStatus = (selector, selector_msg, msc_text)=>{
+    document.querySelector(selector).classList.add("error");
+    document.querySelector(selector_msg).classList.add("error")
+    document.querySelector(selector_msg).innerHTML = msc_text
 
-const {name, surname, adress} = tempUser
-console.log(surname)
+};
 
-delete tempUser.age
-console.log(tempUser)
+const hideErrorStatus = (selector, selector_msg, )=>{
+    document.querySelector(selector).classList.remove("error");
+    document.querySelector(selector_msg).classList.remove("error")
+
+}
+
+// start point
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".form").addEventListener("submit", (e)=>{
+        e.preventDefault();
+        getUserData();
+    });
+});
