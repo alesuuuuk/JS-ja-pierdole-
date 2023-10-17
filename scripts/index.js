d95226c131a7ea9a17aeb85918a39afd9bfe858a
school computer <aslesyk@gmail.com>
2023-10-17 18:56:01 +0300

    noty finish

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,20 @@
-var displayProducts = function (user_name, age) {
-    alert("Hello ".concat(userName, ", you're ").concat(age));
-};
-// introducing syntax and main types
-var userName = "John";
-var age = 15;
-displayProducts(userName, age);
-var answer_for_all_questions_about_TS = "мати рідна то піздєц";
-var isAdult = age > 18;
-alert(answer_for_all_questions_about_TS);
-console.log(age + "12");
-// console.log(age - "12") // error 
-// one test)))))
-// let num_one:  = "213" // dont work
-// let num_one: any = "213"; // work 
-// let num_two = 312;
-// console.log(num_two - num_one); 
-// some special types for TS
-var test2 = 12;
-// let test3: never = true;
-var test4 = null;
-var test5 = undefined;
-// let bignumber: bigint = 0b11111111111111111111111111111111111111111111111111111; // i dont know how this shit works
-var symbol_oop = Symbol("key");
-// _________ ARRAYES__________________
-var fruits = [];
-fruits.push("apple"); // normal 
-// fruits.push(12) // error
-var massive = []; // cant add new element
-// _________ OBJECTS____________
-// it could be but not
-var object1 = {
-    name: "john",
-    age: 12
-};
+// interface AllProducts{
+// }
+var Constants;
+(function (Constants) {
+    Constants["baseURL"] = "https://fakestoreapi.com";
+})(Constants || (Constants = {}));
 ;
-var object = {
-    name: "john",
-    age: 12
+var getAllProducts = function (endpoint) {
+    return fetch("".concat(Constants.baseURL, "/").concat(endpoint))
+        .then(function (res) { return res.json(); })
+        .then(function (json) { json; });
 };
+var displayProducts = function (data) {
+    console.log(data);
+};
+// start point
+document.addEventListener("DOMContentLoaded", function () {
+    var PRODUCTS = getAllProducts('products').then(function (data) { (displayProducts(data)); });
+    console.log(PRODUCTS);
+});
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,62 +1,24 @@
-const displayProducts = (user_name: string, age: number): void => {
-    alert(`Hello ${userName}, you're ${age}`);
+interface Products {
+    id: number,
+    category: string
+}
+
+enum Constants {
+    baseURL = 'https://fakestoreapi.com'
 };
+const getAllProducts = (endpoint: string) => {
+    return fetch(`${Constants.baseURL}/${endpoint}`)
+        .then(res => res.json())
+        .then((json) => { json })
+}
 
-// introducing syntax and main types
-let userName: string | number = "John";
-let age: number = 15;
-displayProducts(userName, age);
-let answer_for_all_questions_about_TS: string = "мати рідна то піздєц";
-let isAdult: boolean = age > 18;
-alert(answer_for_all_questions_about_TS);
-console.log(age + "12");
-// console.log(age - "12") // error 
+const displayProducts = (data: Products[]): void => {
+    console.log(data[0])
+}
 
-// one test)))))
+// start point
+document.addEventListener("DOMContentLoaded", () => {
+    getAllProducts('products').then((data) => { (displayProducts(data)) })
 
-// let num_one:  = "213" // dont work
+});
 
-// let num_one: any = "213"; // work 
-// let num_two = 312;
-// console.log(num_two - num_one); 
-
-
-
-// some special types for TS
-let test2: unknown = 12; // same as any 
-// let test3: never = true; // not usable
-let test4: null = null; 
-let test5: undefined = undefined;
-// let bignumber: bigint = 0b11111111111111111111111111111111111111111111111111111; // i dont know how this shit works
-let symbol_oop: symbol = Symbol("key");
-
-// _________ ARRAYES__________________
-let fruits: string[] = []
-fruits.push("apple") // normal 
-// fruits.push(12) // error
-let massive: readonly string[] = [] // cant add new element
-
-// _________ OBJECTS____________
-// it could be but not
-let object1: {
-    name: string,
-    age: number
-} = {
-    name: "john",
-    age: 12
-};
-
-
-// how its normally shoul look 
-interface userDataTypes {
-    name: string,
-    age: number
-};
-
-let object: userDataTypes=  {
-    name: "john",
-    age: 12
-};
-
-
-// i need to instal node.js and instal TS then with cmd: cd "path to folder"; cd scripts; tsc index.ts
